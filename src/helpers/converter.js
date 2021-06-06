56c8b000c3e3eb3dfec7f4d40f5ae4c552269b4f
olivermanzi <olivermanzi.dev@gmail.com>
2021-06-06 20:19:23 +0200

    makes things prettier
    - adds hashtags to tweet builder
    - removes decimales from conversion rate coin/stock

--- src/helpers/converter.js
+++ src/helpers/converter.js
@@ -1,3 +1,9 @@
+/**
+ * Returns number of stocks that can be bought with one crypto coin
+ * @param {Number} coin - value of coin in USD
+ * @param {Number} stock - value of stock in USD
+ * @returns {Number}
+ */
 exports.CoinToStock = (coin, stock) => {
-	return coin / stock;
+	return Math.floor(coin / stock);
 };
--- src/helpers/tweetBuilder.js
+++ src/helpers/tweetBuilder.js
@@ -1,11 +1,10 @@
 /**
  * Returns a text for stock tweets
- * @param {String} conversion - conversion rate from Eth coin to stock
+ * @param {Number} conversion - conversion rate from Eth coin to stock
  * @param {String} symbol - symbol of stock
  * @param {String} name - name of stock organiztion
  * @returns {String}
  */
 exports.buildStockTweet = (conversion, symbol, name) => {
-	return `One Eth can get you ${conversion} shares of ${symbol}. (a.k.a ${name})`;
+	return `One Eth can get you ${conversion} shares of ${name} (${symbol}). #eth #ethereum #crypto`;
 };
-
